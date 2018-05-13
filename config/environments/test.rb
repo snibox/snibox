62c7a5ad48727507bdd841e6fb7bea35f88052e5
Vladimir Avgustov <vavgustov@gmail.com>
2018-05-13 17:01:16 +0300

    Fix rspec emails.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,6 +33,7 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: ENV['DOMAIN'], port: ENV['PORT'] }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
