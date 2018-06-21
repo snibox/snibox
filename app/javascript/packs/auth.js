9df1cd3f4d85e3237d9aa29f54cecd6139603041
Vladimir Avgustov <vavgustov@gmail.com>
2018-06-21 17:27:43 +0300

    Add ability to flush local storage. Clean up views.

--- app/javascript/packs/auth.js
+++ app/javascript/packs/auth.js
@@ -1,10 +1,23 @@
 import Vue from 'vue/dist/vue.esm'
+import VueToasted from 'vue-toasted'
+
+Vue.use(VueToasted, {
+  duration: 3000,
+  position: 'bottom-right'
+})
 
 document.addEventListener('DOMContentLoaded', () => {
   window.navbar = new Vue({
     el: '#auth-wrapper',
     mounted() {
       this.$el.style.display = 'block'
+    },
+    methods: {
+      resetLocalStorage() {
+        localStorage.removeItem('labels_active')
+        localStorage.removeItem('label_snippets_active')
+        this.$toasted.success('Local storage flushed!')
+      }
     }
   })
 })
