863ab1e69a83cfdd9476c58736d49dc0b633efab
Vladimir Avgustov <vavgustov@gmail.com>
2018-06-27 21:27:03 +0300

    Increase max snippet length to 20k.

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -6,7 +6,7 @@ class Snippet < ApplicationRecord
   accepts_nested_attributes_for :label
 
   validates :title, presence: true, length: { maximum: 40 }
-  validates :content, presence: true, length: { maximum: 10000 }
+  validates :content, presence: true, length: { maximum: 20000 }
   validates :language, presence: true, inclusion: { in: Editor.languages.keys.map(&:to_s) }
   validates :tabs, numericality: { only_integer: true }, inclusion: { in: [2, 4, 8] }
 
