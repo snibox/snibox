c2e3cd42af54cb333a9f361cd00ad6f947b247d7
Vladimir Avgustov <vavgustov@gmail.com>
2018-07-05 11:30:47 +0300

    Rebuild demo instance.

--- app/javascript/packs/repository.js
+++ app/javascript/packs/repository.js
@@ -1,3 +1,5 @@
+// this line will rebuild demo on Heroku
+// TODO: remove the comment above
 import Vue from 'vue/dist/vue.esm'
 import VueHighlightJS from 'vue-highlightjs'
 import store from '../snibox/store'
