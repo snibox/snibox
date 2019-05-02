7d83d4c400da0c649a923f18a47def8bcb5c3a69
Vladimir Avgustov <vavgustov@gmail.com>
2019-05-03 01:14:11 +0300

    Use 'main' as default file name for snippet -> snippet file conversion.

--- db/migrate/20190401062316_convert_snippets_to_snippet_files.rb
+++ db/migrate/20190401062316_convert_snippets_to_snippet_files.rb
@@ -1,15 +1,13 @@
 class ConvertSnippetsToSnippetFiles < ActiveRecord::Migration[5.2]
   def change
     Snippet.all.each do |snippet|
-      snippet_file = SnippetFile.new
-      snippet_file.update(
-        title: snippet.title,
+      SnippetFile.create!(
+        title: 'main',
         content: snippet.content,
         language: snippet.language,
         tabs: snippet.tabs,
         snippet: snippet
       )
-      snippet_file.save!
     end
 
     remove_column :snippets, :content
