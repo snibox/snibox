96865ce24366c4b9d22d26b4c2e6c727d19b5ec9
Vladimir Avgustov <vavgustov@gmail.com>
2019-05-31 20:23:32 +0300

    Improved backend performance.

--- app/controllers/api/v1/data_controller.rb
+++ app/controllers/api/v1/data_controller.rb
@@ -1,9 +1,12 @@
 class Api::V1::DataController < Api::BaseController
   def default_state
+    snippets = Snippet.includes(:snippet_files, :label)
+
     data = {
-      snippets: ActiveModelSerializers::SerializableResource.new(Snippet.all, each_serializer: ::SnippetSerializer),
+      snippets: ActiveModelSerializers::SerializableResource.new(snippets, each_serializer: ::SnippetSerializer),
       languages: Editor.languages
     }
+
     render json: data
   end
 end
